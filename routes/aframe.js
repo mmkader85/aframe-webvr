d907e74199f98214d8b198930dc00f4c682274e4
Mohamed Abdul Kader <mmkader85@gmail.com>
2017-05-01 12:58:54 +0800

    Basic and Scene built using webvr

---
+++ routes/aframe.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mmkad on 01-May-17.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/basic', function(req, res, next) {
+    res.render(
+        'basic',
+        {
+            'title': 'Basic aframe webvr'
+        }
+    );
+});
+
+router.get('/scene', function(req, res, next) {
+    res.render(
+        'scene',
+        {
+            'title': 'Build scene in VR using aframe webvr'
+        }
+    );
+});
+
+module.exports = router;
